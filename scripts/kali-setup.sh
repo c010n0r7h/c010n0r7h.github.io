fe124fe3dc0d7737bce893e519f87e6561fa9e17
c010n0r7h <35034837+c010n0r7h@users.noreply.github.com>
2018-01-07 13:42:43 -0600

    Create kali-setup.sh

---
+++ scripts/kali-setup.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#-------------------------------------------------------------#
+# Filename: kali-setup.sh                                     #
+# Kali post-install script              (updated: 2017-12-28  #
+#                                                             #
+
+
+#-(Cosmetic) Colour output------------------------------------#
+RED="\033[01;31m" 	# Issues/Errors
+GREEN="\033[01;32m" 	# Success
+YELLOW="\033[01;33m" 	# Warnings/Information
+BLUE="\033[01;34m" 	# Heading
+BOLD="\033[01;01m" 	# Highlight
+RESET="\033[00m" 	# Normal  
+
+#-Change Password---------------------------------------------#
+passwd                  
+
+
+#-Setup SSH---------------------------------------------------#
+#service ssh start		#Start SSH service
+#netstat -antp|grep sshd	#Verify SSH is running
+#update-rc.d ssh enable		#Start SSH automatically at boot
+
+
+#-Setup HTTP Service (web directory root: /var/www)-----------#
+#service apache2 start		#Start Apache service
+#netstat -antp|grep apache	#Verify Apache is running
+#update-rc.d apache2 enable	#Start Apache automatically at boot
